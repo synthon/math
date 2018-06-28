d736c8b42047a54a28f91c3ec33fb781fdce8052
WalterBishopPhD <40410117+WalterBishopPhD@users.noreply.github.com>
2018-06-28 10:03:07 +0300

    Add files via upload

--- challenge.js
+++ challenge.js
@@ -1,4 +1,4 @@
-function startPrimer() {
+function startCount() {
   let number1 = Math.round(Math.random()*100);
   let number2 = Math.round(Math.random()*100);
   document.querySelector(".example").value = `${number1} + ${number2}`;
@@ -9,27 +9,32 @@ function resultCheck(ke) {
   const answer = document.querySelector(".answer");
   const score = document.querySelector("#score");
   const key = ke.key;
+
   if ("0123456789".includes(key)) {
     (answer === document.activeElement) 
     ? answer.value = answer.value
     : answer.value += key;
   }
+
   if (["Backspace", "Delete"].includes(key)) {
-    answer.value = answer.value.slice(this.length, -1);
+    (answer === document.activeElement) 
+    ? answer.value = answer.value
+    : answer.value = answer.value.slice(this.length, -1);
   }
+
   if (["Enter"].includes(key)) {
-    let points = parseInt(score.textContent);
+    const points = parseInt(score.textContent);
 
     (eval(display) === parseInt(answer.value))
     ? score.textContent = points + 1
     : score.textContent = points - 2;
     answer.value = "";
-    startPrimer();
+    startCount();
   }
 }
 document.addEventListener("keydown", resultCheck);
 // document.addEventListener("click", (e) => {console.log(e)});
 
 (function() {
-  startPrimer();
+  startCount();
 })();

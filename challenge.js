33655a50e6f9c9219e827b7f79e2661effe801d9
WalterBishopPhD <40410117+WalterBishopPhD@users.noreply.github.com>
2018-06-28 06:50:04 +0300

    Add files via upload

---
+++ challenge.js
@@ -0,0 +1,31 @@
+function startPrimer() {
+  let number1 = Math.round(Math.random()*100);
+  let number2 = Math.round(Math.random()*100);
+  document.querySelector(".example").value = `${number1} + ${number2}`;
+}
+
+function resultCheck(ke) {
+  const display = document.querySelector(".example").value;
+  const answer = document.querySelector(".answer");
+  const score = document.querySelector("#score");
+  const key = ke.key;
+  if ("0123456789".includes(key)) {
+    answer.value += key;
+  }
+  if (["Backspace", "Delete", "CE"].includes(key)) {
+    answer.value = answer.value.slice(this.length, -1);
+  }
+  if (["Enter"].includes(key)) {
+    (eval(display) === parseInt(answer.value))
+    ? score.textContent += 1
+    : score.textContent -= 2;
+    answer.value = "";
+    startPrimer();
+  }
+}
+document.addEventListener("keydown", resultCheck);
+// document.addEventListener("click", (e) => {console.log(e)});
+
+(function() {
+  startPrimer();
+})();

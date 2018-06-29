557560596467a341f243fa3197ea6a977d7cedeb
WalterBishopPhD <40410117+WalterBishopPhD@users.noreply.github.com>
2018-06-29 04:00:09 +0300

    Add files via upload

--- challenge.js
+++ challenge.js
@@ -1,8 +1,8 @@
 const display = document.querySelector(".example"),
-      answer = document.querySelector(".answer"),
-      score = document.querySelector("#score"),
-      start = document.querySelector(".start");
-let   timer = 30;
+      answer  = document.querySelector(".answer"),
+      score   = document.querySelector("#score"),
+      start   = document.querySelector(".start");
+let   timer   = 30;
 
 function startCount() {
   let min = 11,
@@ -19,25 +19,25 @@ function resultCheck(ke) {
   const key = ke.key;
 
   if ("0123456789".includes(key)) {
-    answer === document.activeElement
-      ? answer.value = answer.value
-      : answer.value += key;
+      answer === document.activeElement
+        ? answer.value = answer.value
+        : answer.value += key;
   }
 
   if (["Backspace", "Delete"].includes(key)) {
-    answer === document.activeElement
-      ? answer.value = answer.value
-      : answer.value = answer.value.slice(this.length, -1);
+      answer === document.activeElement
+        ? answer.value = answer.value
+        : answer.value = answer.value.slice(this.length, -1);
   }
 
   if (["Enter"].includes(key)) {
-    points = parseInt(score.textContent);
+      points = parseInt(score.textContent);
 
-    eval(display.value) === parseInt(answer.value)
-      ? score.textContent = points + 1
-      : score.textContent = points - 2;
-    answer.value = "";
-    startCount();
+      eval(display.value) === parseInt(answer.value)
+        ? score.textContent = points + 1
+        : score.textContent = points - 2;
+      answer.value = "";
+      startCount();
   }
 }
 document.addEventListener("keydown", resultCheck);
@@ -46,13 +46,13 @@ start.onclick = function timerStart() {
   document.querySelector(".timer").textContent = timer;
 
   if (timer < 0) {
-    alert("Time's UP!");
-    timer = 30;
-    score.textContent = "0";
+      alert("Time's UP!");
+      timer = 30;
+      score.textContent = "0";
   }
   else {
-    setTimeout(timerStart, 1000);
-    --timer;
+      setTimeout(timerStart, 1000);
+      --timer;
   }
 }
 // document.addEventListener("click", (e) => {console.log(e)});
